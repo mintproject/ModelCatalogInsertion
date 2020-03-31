a5063f3bb4c043fdad975d03f7fcfae804225e57
Dhruv <dhruvpatel5738@gmail.com>
2020-03-30 22:20:29 -0700

    Integrated nose testing for CLI Testing

---
+++ src/caps/tests/run_tests.py
@@ -0,0 +1,27 @@
+import unittest
+import logging
+import logging.config
+
+from pathlib import Path
+from flask_testing import TestCase
+from click.testing import CliRunner
+from src.caps import __main__
+
+logging_file = Path(__file__).parent.parent / "settings" / "logging.ini"
+try:
+    logging.config.fileConfig(logging_file)
+except:
+    logging.error("Logging config file does not exist {}".format(logging_file))
+    exit(0)
+logger = logging.getLogger(__name__)
+
+class TestCapsCliCommands(unittest.TestCase):
+    def test_configure_command(self):
+        runner = CliRunner()
+        logger.info("hello")
+        result = runner.invoke(__main__.configure, input="mlsnfklsd\nkjahdkasj")
+        logger.debug(result)
+        self.assertEqual(result.exit_code, 0)
+
+if __name__ == '__main__':
+    unittest.main()
--- src/caps/tests/test_schema.py
+++
@@ -1,2 +0,0 @@
-def test_schema_fail():
-    assert True
